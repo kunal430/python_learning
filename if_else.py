2202cea763e0ebba75729bb0b0eebdc8cf9aa1d1
kunal chavan <153481257+kunal430@users.noreply.github.com>
2026-02-18 20:00:17 +0530

    Add files via upload
    add file

---
+++ if_else.py
@@ -0,0 +1,7 @@
+gender=input("enter your gendr as m or f=")
+if gender == "m" or gender == "M":
+    print("hii sir")
+elif gender == "f" or gender == "F" :
+    print("hii maam")
+else:
+    print("preferd not to say")
