6167751b40a22b5248afa05216ea107ac3507fc6
kunal chavan <153481257+kunal430@users.noreply.github.com>
2026-02-19 08:41:30 +0530

    Add files via upload
    Add code file

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+num1=int(input("frist num="))
+num2=int(input("second num="))
+if num1 > num2:
+    print("frist is grater")
+elif num2 > num1:
+    print("second is grater")
+else:
+    print("both are equal")
