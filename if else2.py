ae9d3d6864ae40720ba694e70792738950c2e2cd
kunal chavan <153481257+kunal430@users.noreply.github.com>
2026-02-20 10:21:45 +0530

    Add file

---
+++ if else2.py
@@ -0,0 +1,7 @@
+name=input("enter your age = ")
+age=int(input("enter your age = "))
+
+if age >=18:
+    print(f"hello {name} you are eligible to vote ")
+else:
+    print(f"hello {name} you are not eligible")
