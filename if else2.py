49775c434104a23628103ccce1805bd9ed188bf7
kunal chavan <153481257+kunal430@users.noreply.github.com>
2026-02-20 11:52:09 +0530

    add comments

--- if else2.py
+++ if else2.py
@@ -1,7 +1,10 @@
-name=input("enter your age = ")
+name=input("enter your age = ")  #user input 
 age=int(input("enter your age = "))
 
-if age >=18:
+if age >=18: 
     print(f"hello {name} you are eligible to vote ")
 else:
     print(f"hello {name} you are not eligible")
+
+
+#this program will generate o/p by comparing age amd generate o/p 
